423321f81f2db4f56baf4c83646e2ad97226cb29
Плюснин Евгений Андреевич <eaplyusnin@sberautotech.ru>
2022-09-06 18:21:58 +0300

    Did listing_3_9 and exercise_3_1

---
+++ PART_I/lesson3/exercise_3_1.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+
+using namespace std;
+
+enum YourCards {
+    Ace = 43, 
+    Jack, 
+    Queen,
+    King,
+};
+
+int main(int argc, char const *argv[])
+{
+    /* code */
+    cout << int(Queen) << endl;
+    return 0;
+}
+
---
+++ PART_I/lesson3/listing_3_9.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+
+enum CardinalDirections {
+    North = 25, 
+    South, 
+    East,
+    West,
+};
+
+int main(int argc, char const *argv[])
+{
+    cout << North << endl;
+    cout << South << endl;
+    return 0;
+}
