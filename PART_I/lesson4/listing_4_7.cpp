296ecb28533f3fd92fdce4abd5e8ea6ff388d841
EvgeniiPliusnin <eaplyusnin@sberautotech.ru>
2022-09-20 12:02:32 +0300

    check commit

---
+++ PART_I/lesson4/listing_4_7.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+int main(int argc, char const *argv[])
+{
+    string greet_string ("Hello std::string!");
+    cout << greet_string << endl;
+    return 0;
+}
