5e00846aa5dd014bca48b5ffb30e369a9bffa213
EvgeniiPliusnin <plyusnin-ea@mail.ru>
2022-09-20 12:07:44 +0300

    fix

--- PART_I/lesson4/listing_4_7.cpp
+++ PART_I/lesson4/listing_4_7.cpp
@@ -2,10 +2,12 @@
 #include <string>
 
 using namespace std;
-
 int main(int argc, char const *argv[])
 {
     string greet_string ("Hello std::string!");
     cout << greet_string << endl;
+
+    cout << "Enter a line of text";
+
     return 0;
 }
