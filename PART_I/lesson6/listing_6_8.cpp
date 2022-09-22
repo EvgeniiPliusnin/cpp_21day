7e8ee08d338e7d426ca82a69618ad614266b7fb5
EvgeniiPliusnin <plyusnin-ea@mail.ru>
2022-09-22 14:32:27 +0300

    Done listing_6_8 while loop

---
+++ PART_I/lesson6/listing_6_8.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+
+int main(int argc, char const *argv[])
+{
+    bool temp_val = false;
+    while (temp_val)
+    {
+       cout << temp_val << endl; 
+    }
+    
+    return 0;
+}
