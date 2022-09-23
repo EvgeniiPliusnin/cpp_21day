67ce5f1d583436cac89cf97105eedf61a49368ed
EvgeniiPliusnin <ea.plyusnin@gmail.com>
2022-09-23 21:37:34 +0300

    Done example with Fibonacci listing_7_5

---
+++ PART_I/lesson7/listing_7_5_Fibonacci.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+using namespace std;
+
+int get_fib_number(int fib_index) {
+    if(fib_index < 2) {
+        return fib_index;
+    } else {
+        return get_fib_number(fib_index - 1) + get_fib_number(fib_index - 2);
+    }
+}
+
+int main(int argc, char const *argv[])
+{
+    cout << "Enter 0-based index of desired Fibonacci Number: ";
+    int index = 0;
+    cin >> index;
+
+    cout << "Fibonacci number is: " << get_fib_number(index) << endl;
+    return 0;
+}
