3cce30625840bf7f322f72ffee392696d0cf61a8
EvgeniiPliusnin <ea.plyusnin@gmail.com>
2022-10-03 22:13:55 +0500

    done listing_9_18

---
+++ PART_II/lesson9/listing_9_18.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+using namespace std;
+
+class Human {
+    int age;
+
+public:
+    constexpr Human(int human_age) : age (human_age) {}
+    constexpr int get_age() const {return age;}
+};
+
+int main(int argc, char const *argv[])
+{
+    constexpr Human some_person(15);
+    const int his_age = some_person.get_age();
+
+    return 0;
+}
