e0829484bb11a9936289a6623a170953705f3488
EvgeniiPliusnin <ea.plyusnin@gmail.com>
2022-10-04 00:39:47 +0500

    done listing_10_5

---
+++ PART_II/lesson10/listing_10_5.cpp
@@ -0,0 +1,36 @@
+#include <iostream>
+using namespace std;
+
+class Fish {
+    private:
+        bool is_fresh_water_fish;
+
+    public:
+        Fish(bool is_fresh) : is_fresh_water_fish(is_fresh) {}
+
+        void swim() {
+            if(is_fresh_water_fish) {
+                cout << "fish swims in lake" << endl;
+            } else {
+                cout << "fish swims in sea" << endl;
+            }
+        }
+};
+
+class Tuna : public Fish {
+    public:
+        Tuna() : Fish (false) {}
+
+        void swim() {
+            cout << "Tuna swims real fast" << endl;
+            Fish::swim();
+        }
+};
+
+int main(int argc, char const *argv[])
+{
+    Tuna lunch;
+    lunch.swim();
+
+    return 0;
+}
