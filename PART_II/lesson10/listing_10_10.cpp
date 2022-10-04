36e1906d457b11a1325507a9c6d4c42033235535
EvgeniiPliusnin <ea.plyusnin@gmail.com>
2022-10-04 22:28:51 +0300

    done listing_10_10 (multiply inheritance)

---
+++ PART_II/lesson10/listing_10_10.cpp
@@ -0,0 +1,40 @@
+#include <iostream>
+
+class Mammal {
+    public:
+        void feed_baby_milk () {
+            std::cout << "Mammal: Baby says glug!" << std::endl;
+        }
+};
+
+class Reptile {
+    public:
+        void spit_venom() {
+            std::cout << "Reptile: Shoo enemy! Spit venom!" << std::endl;
+        }
+};
+
+class Bird {
+    public:
+        void lay_eggs() {
+            std::cout << "Bird: Laid my eggs, am lighter now!" << std::endl;
+        }
+};
+
+class Platupys : public Mammal, public Bird, public Reptile {
+    public:
+        void swim() {
+            std::cout << "Platupys: Voily, I can swim!" << std::endl;
+        }
+};
+
+int main(int argc, char const *argv[])
+{
+    Platupys real_freak;
+    real_freak.lay_eggs();
+    real_freak.feed_baby_milk();
+    real_freak.spit_venom();
+    real_freak.swim();
+
+    return 0;
+}
