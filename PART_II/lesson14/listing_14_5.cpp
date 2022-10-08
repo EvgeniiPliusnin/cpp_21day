8ca15857ced40b35a6911968a69cc054a46ce124
EvgeniiPliusnin <ea.plyusnin@gmail.com>
2022-10-08 21:48:22 +0300

    add template specialization for double type in class HoldsPair

--- PART_II/lesson14/listing_14_5.cpp
+++ PART_II/lesson14/listing_14_5.cpp
@@ -35,11 +35,35 @@ template<> class HoldsPair<int, int> {
         }
 };
 
+template<> class HoldsPair<double, double> {
+    private:
+        double value1;
+        double value2;
+
+    public:
+        HoldsPair(const double& val1, const double& val2) : value1(val1), value2(val2) {}
+
+        const double& get_first_value() const {
+            std::cout << "returning double " << value1 << std::endl;
+            return value1;
+        }
+
+        const double& get_second_value() const {
+            std::cout << "returning double " << value2 << std::endl;
+            return value2;
+        }
+};
+
+
 int main(int argc, char const *argv[])
 {
     HoldsPair<int, int> pair_int_int(222, 333); 
     pair_int_int.get_first_value();
     pair_int_int.get_second_value();
+
+    HoldsPair<double, double> pair_db_db(123.01, 1259.07);
+    pair_db_db.get_first_value();
+    pair_db_db.get_second_value();
 
     return 0;
 }
