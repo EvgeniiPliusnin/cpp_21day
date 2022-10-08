1a80fc0bc194d23aef614e50e6c498bf88758053
EvgeniiPliusnin <ea.plyusnin@gmail.com>
2022-10-08 21:43:18 +0300

    done listing_14_5 (template specialization)

---
+++ PART_II/lesson14/listing_14_5.cpp
@@ -0,0 +1,45 @@
+#include <iostream>
+
+template <typename T1 = int, typename T2 = double>
+class HoldsPair {
+    private:
+        T1 value1;
+        T2 value2;
+
+    public:
+        HoldsPair(const T1& val1, const T2& val2) : value1(val1), value2(val2) {}
+
+        // accessor functions
+        const T1& get_first_value() const;
+        const T2& get_second_value() const;
+};
+
+// specialization of HoldPairs class for types int & int here
+template<> class HoldsPair<int, int> {
+    private:
+        int value1;
+        int value2;
+
+    public:
+        HoldsPair(const int& val1, const int& val2) : value1(val1), value2(val2) {}
+
+        // accessor functions
+        const int& get_first_value() const {
+            std::cout << "returning integer " << value1 << std::endl;
+            return value1;
+        }
+
+        const int& get_second_value() const {
+            std::cout << "returning integer " << value2 << std::endl;
+            return value2;
+        }
+};
+
+int main(int argc, char const *argv[])
+{
+    HoldsPair<int, int> pair_int_int(222, 333); 
+    pair_int_int.get_first_value();
+    pair_int_int.get_second_value();
+
+    return 0;
+}
